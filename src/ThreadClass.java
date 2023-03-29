c14fcd3747f21a8ae52113ce7d21416d8cf14ffb
GRISONRF <grisonrf@gmail.com>
2023-03-29 13:51:01 -0600

    multithread with thread class

--- src/ThreadClass.java
+++ src/ThreadClass.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class ThreadClass {
+
+//this is a subclass of the thread class, it gives all the attributes and the behavior that it should have to become a Java thread.
+public class ThreadClass extends Thread {
+
+    private int number;
+
+    public ThreadClass(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public void run(){
+        for(int i = 1; i <= 5; i++){
+            System.out.println(i + " from the thread " + number);
+        }
+    }
+
 }
