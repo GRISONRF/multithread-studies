422033f643f69e321b11b23487fe73fe528b9ba8
GRISONRF <grisonrf@gmail.com>
2023-04-04 10:31:52 -0600

    testing to use the sleep method in the thread

--- src/RunnableInterface.java
+++ src/RunnableInterface.java
@@ -8,6 +8,11 @@ public class RunnableInterface implements Runnable{
     public void run() {
         for(int i=1; i<=5; i++){
             System.out.println(i);
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
